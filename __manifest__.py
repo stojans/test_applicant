349e3a7181e3e77d93c704b39e897f5c09efb0e3
stefan <stefann.stojanovic@gmail.com>
2025-02-23 00:01:44 +0100

    Edit init and manifest files

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Test Applicant Module',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': 'Custom Test Applicant module for managing applicants.',
+    'author': 'Stefan',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/test_module_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
