f31cfea1b25fdc4ceb3a063dad3be64da0f98887
stefan <stefann.stojanovic@gmail.com>
2025-02-23 19:23:30 +0100

    Add POST route

--- controllers/main.py
+++ controllers/main.py
@@ -2,6 +2,7 @@ from odoo import http
 from odoo.http import request, Response
 import json
 
+
 class TestModelController(http.Controller):
     @http.route('/api/test-model', auth='public', methods=['GET'], csrf=False)
     def get_test_model(self):
@@ -23,3 +24,33 @@ class TestModelController(http.Controller):
         headers = {'Content-Type': 'application/json'}
 
         return Response(json.dumps(result),headers=headers)
+
+
+    @http.route('/api/test-model', type='json', auth='user', methods=['POST'], csrf=False)
+    def create_test_model(self, **post):
+        post = request.httprequest.get_json()
+        name = post.get('name', None)
+        description = post.get('description', None)
+        state = post.get('state', 'draft')
+
+        if not name:
+            return {"error": "Name is required."}
+
+        new_record = request.env['test.model'].create({
+            'name': name,
+            'state': state,
+            'description': description,
+
+        })
+
+        headers = {'Content-Type': 'application/json'}
+
+        return Response(
+            json.dumps({
+                'status': 'success',
+                'id': new_record.id,
+                'name': new_record.name,
+                'state': new_record.state,
+            }),
+            headers=headers,
+        )
